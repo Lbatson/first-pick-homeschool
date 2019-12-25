8db55664f8f0bfce0329518fcb5da825c355d655
Lance Batson <lancebatsondev@gmail.com>
2019-12-24 21:32:29 -0600

    add attribute models to curriculums

--- app/curriculums/admin.py
+++ app/curriculums/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Curriculum
+from .models import Curriculum, Category, Subject, Grade, Level, Age
 
 # Register your models here.
 admin.site.register(Curriculum)
+admin.site.register(Category)
+admin.site.register(Subject)
+admin.site.register(Grade)
+admin.site.register(Level)
+admin.site.register(Age)
--- app/curriculums/models.py
+++ app/curriculums/models.py
@@ -2,10 +2,50 @@ from django.db import models
 
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Grade(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Level(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+
+class Age(models.Model):
+    name = models.CharField(max_length=3)
+
+    def __str__(self):
+        return self.name
+
+
 class Curriculum(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(max_length=2000)
     is_confirmed = models.BooleanField(default=False)
+    categories = models.ManyToManyField(Category, related_name='curriculums')
+    subjects = models.ManyToManyField(Subject, related_name='curriculums')
+    grades = models.ManyToManyField(Grade, related_name='curriculums')
+    levels = models.ManyToManyField(Level, related_name='curriculums')
+    ages = models.ManyToManyField(Age, related_name='curriculums')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
