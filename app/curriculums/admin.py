3f7ce8b20b390085e7047eaa32383505ed820777
Lance Batson <lancebatsondev@gmail.com>
2020-01-04 16:30:02 -0600

    update admin display

--- app/curriculums/admin.py
+++ app/curriculums/admin.py
@@ -3,14 +3,33 @@ from .models import Curriculum, Category, Subject, Grade, Level, Age, Publisher
 
 
 class CurriculumAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'is_confirmed')
+    list_display = ('id', 'name', 'description', 'link', 'is_confirmed')
+    list_display_links = ('name',)
+    list_filter = (
+        'is_confirmed',
+        'subjects__category',
+        'subjects',
+        'grades',
+        'levels',
+        'ages'
+    )
+
+
+class PublisherAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'link')
+    list_display_links = ('name',)
+
+
+class SubjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category')
+    list_display_links = ('name',)
 
 
 # Register your models here.
 admin.site.register(Curriculum, CurriculumAdmin)
+admin.site.register(Publisher, PublisherAdmin)
+admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Category)
-admin.site.register(Subject)
 admin.site.register(Grade)
 admin.site.register(Level)
 admin.site.register(Age)
-admin.site.register(Publisher)
