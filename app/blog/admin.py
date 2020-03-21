5e6f31d01f3f2f20fadeb1b5e73cd93bb220e367
Lance Batson <lancebatsondev@gmail.com>
2020-03-21 17:45:23 -0500

    add index and about page. add blog app

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Article, Category
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug', 'category',
+                    'created_by', 'is_confirmed')
+    list_display_links = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'slug')
+    list_display_links = ('name',)
+
+
 # Register your models here.
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Category, CategoryAdmin)
--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,41 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    body = models.TextField()
+    is_confirmed = models.BooleanField(default=False)
+    category = models.ForeignKey(
+        'blog.Category',
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='articles'
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='articles'
+    )
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.title
