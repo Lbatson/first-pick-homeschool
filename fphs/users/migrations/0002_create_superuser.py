bbdf41cd04121ec909bdc7f461a807666ea6f7aa
Lance Batson <lancebatsondev@gmail.com>
2020-07-11 08:44:00 -0500

    add initial superuser migration

---
+++ fphs/users/migrations/0002_create_superuser.py
@@ -0,0 +1,23 @@
+from config.settings.base import ADMIN_EMAIL, ADMIN_USERNAME, ADMIN_PASSWORD
+from django.contrib.auth import get_user_model
+from django.db import migrations
+
+def create_superuser(apps, schema_editor):
+    superuser = get_user_model().objects.create_superuser(
+        email=ADMIN_EMAIL,
+        username=ADMIN_USERNAME,
+        password=ADMIN_PASSWORD
+    )
+
+    superuser.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser)
+    ]
